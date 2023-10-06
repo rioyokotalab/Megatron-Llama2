eae5d8b222e165146429b913a5cb0d20857c2c34
kazuki <kaz.tokyo.tech20@gmail.com>
2023-10-06 18:27:41 +0900

    env: add installation script

---
+++ scripts/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#$ -l rt_AG.small=1
+#$ -l h_rt=1:00:00
+#$ -j y
+#$ -o outputs/install/
+#$ -cwd
+
+# module load
+source /etc/profile.d/modules.sh
+module load cuda/11.8/11.8.0
+module load cudnn/8.9/8.9.2
+module load nccl/2.16/2.16.2-1
+module load hpcx/2.12
+
+# python virtualenv
+cd /bb/llm/gaf51275/llama/Megatron-LM
+source .env/bin/activate
+
+# pip install
+pip install --upgrade pip
+pip install -r requirements.txt
+
+pip install ninja wheel packaging
+
+# apex install
+git clone git@github.com:NVIDIA/apex.git
+cd apex
+git checkout 50ac8425403b98147cbb66aea9a2a27dd3fe7673
+
+pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./
+
+# flash-attention install
+pip install flash-attn==2.3.0 --no-build-isolation
