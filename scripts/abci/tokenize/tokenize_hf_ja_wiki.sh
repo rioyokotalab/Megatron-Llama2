6f7e73d0e0657a9fdcaf3ae4f2b72cc411e6f057
kazuki <kaz.tokyo.tech20@gmail.com>
2023-10-13 10:38:34 +0900

    feat: tokenize script

---
+++ scripts/abci/tokenize/tokenize_hf_ja_wiki.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#$ -l rt_AG.small=1
+#$ -l h_rt=5:00:00
+#$ -j y
+#$ -o outputs/tokenize/
+#$ -cwd
+
+# module load
+source /etc/profile.d/modules.sh
+module load cuda/11.8/11.8.0
+module load cudnn/8.9/8.9.2
+module load nccl/2.16/2.16.2-1
+module load hpcx/2.12
+
+# python virtualenv
+cd /bb/llm/gaf51275/llama/Megatron-LM
+source .env/bin/activate
+
+DATASET_DIR=/bb/llm/gaf51275/llm-jp/datasets/llm-jp-corpus/108b-merged
+OUTPUT_DIR=/bb/llm/gaf51275/llama/datasets/llama2-llm-jp-corpus/v1.0.2/tokenized/llama-2-hf-tokenizer
+
+mkdir -p ${OUTPUT_DIR}
+
+# tokenize japanese wikipedia
+python tools/preprocess_data.py \
+  --input ${DATASET_DIR}/ja_wiki.jsonl \
+  --output-prefix ${OUTPUT_DIR}/ja_wiki \
+  --tokenizer-type Llama2Tokenizer \
+  --tokenizer-model /bb/llm/gaf51275/llama/huggingface-checkpoint/Llama-2-7b-hf/tokenizer.model \
+  --append-eod \
+  --workers 64
