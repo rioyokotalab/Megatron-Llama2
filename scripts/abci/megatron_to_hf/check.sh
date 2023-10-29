a1e865166d3a93ae53c1455e42b945d98ca5ca19
kazuki <kaz.tokyo.tech20@gmail.com>
2023-10-29 16:01:46 +0900

    feat: convert result check script

---
+++ scripts/abci/megatron_to_hf/check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#$ -l rt_AF=1
+#$ -l h_rt=5:00:00
+#$ -j y
+#$ -o outputs/check/
+#$ -cwd
+
+# module load
+source /etc/profile.d/modules.sh
+module load cuda/11.8/11.8.0
+module load cudnn/8.9/8.9.2
+module load nccl/2.16/2.16.2-1
+module load hpcx/2.12
+
+# python virtualenv
+cd /bb/llm/gaf51275/llama/Megatron-LM
+source .env/bin/activate
+
+python scripts/abci/megatron_to_hf/check.py
