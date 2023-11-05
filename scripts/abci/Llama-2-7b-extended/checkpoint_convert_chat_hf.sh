1bd06081b55b4b17a20c9bfa47fea409a0b87170
kazuki <kaz.tokyo.tech20@gmail.com>
2023-11-05 15:46:59 +0900

    feat: change directory & delete unneeded scripts

---
+++ scripts/abci/Llama-2-7b-extended/checkpoint_convert_chat_hf.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#$ -l rt_AF=1
+#$ -l h_rt=5:00:00
+#$ -j y
+#$ -o outputs/checkpoint_convert/
+#$ -cwd
+
+# module load
+source /etc/profile.d/modules.sh
+module load cuda/11.8/11.8.0
+module load cudnn/8.9/8.9.2
+module load nccl/2.16/2.16.2-1
+module load hpcx/2.12
+
+# python virtualenv
+cd /bb/llm/gaf51275/llama/Megatron-LM
+source .env/bin/activate
+
+# distributed settings
+TENSOR_PARALLEL_SIZE=2   # fixed
+PIPELINE_PARALLEL_SIZE=2 # num layers 32: Llama-2 7B
+
+# model config
+HF_CHECKPOINT_DIR=/bb/llm/gaf51275/llama/huggingface-checkpoint/Llama-2-7b-chat-hf
+MEGATRON_CHECKPOINT_DIR=/bb/llm/gaf51275/llama/llama-megatron-convert-checkpoint-hf/Llama-2-7b-chat/tp${TENSOR_PARALLEL_SIZE}-pp${PIPELINE_PARALLEL_SIZE}
+
+mkdir -p ${MEGATRON_CHECKPOINT_DIR}
+
+# tokenizer config
+TOKENIZER_MODEL=/bb/llm/gaf51275/llama/huggingface-checkpoint/Llama-2-7b-chat-hf/tokenizer.model
+
+# convert
+python tools/checkpoint/util.py \
+  --model-type GPT \
+  --loader llama2_hf \
+  --saver megatron \
+  --target-tensor-parallel-size ${TENSOR_PARALLEL_SIZE} \
+  --target-pipeline-parallel-size ${PIPELINE_PARALLEL_SIZE} \
+  --load-dir ${HF_CHECKPOINT_DIR} \
+  --save-dir ${MEGATRON_CHECKPOINT_DIR} \
+  --tokenizer-model ${TOKENIZER_MODEL}
