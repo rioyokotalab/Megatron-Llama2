9401a136ff2d72011c1ead9e3e99701921d43be2
kazuki <kaz.tokyo.tech20@gmail.com>
2023-11-07 12:37:10 +0900

    Update save interval to 250.

--- scripts/abci/Llama-2-70B-extended/32node/llama-2-70b-base-cc-dp4-tp8-pp8.sh
+++ scripts/abci/Llama-2-70B-extended/32node/llama-2-70b-base-cc-dp4-tp8-pp8.sh
@@ -155,7 +155,7 @@ mpirun -np $NUM_GPUS \
   --adam-beta1 0.9 \
   --adam-beta2 0.95 \
   --log-interval 1 \
-  --save-interval 500 \
+  --save-interval 250 \
   --eval-interval 100 \
   --eval-iters 10 \
   --bf16 \
