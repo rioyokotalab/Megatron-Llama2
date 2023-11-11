cbb384f10e4e846695055d799cdaf6bbccd3535a
kazuki <kaz.tokyo.tech20@gmail.com>
2023-11-11 15:16:57 +0900

    feat: megatron -> hf convert script

--- scripts/ylab/megatron_to_hf/convert-70b-extended.sh
+++ scripts/ylab/megatron_to_hf/convert-70b-extended.sh
@@ -17,16 +17,22 @@ cd /home/kazuki/llama/Megatron-LM
 source .env/bin/activate
 
 # TP > 1, PP > 1の場合は、TP=1, PP=1になるように scripts/abci/change_tp_pp.sh を実行してからconvertしてください
-BASE_TENSOR_PARALLEL_SIZE=1  # fixed
+BASE_TENSOR_PARALLEL_SIZE=1   # fixed
 BASE_PIPELINE_PARALLEL_SIZE=1 # fixed
 
-SAVE_DIR=/mnt/nfs/Users/kazuki/checkpoints/llama/huggingface-checkpoint/Llama-2-70b-megatron/tp8-pp8/
+ITERATION=2000
+FORMATTED_ITERATION=$(printf "%07d" $ITERATION)
+
+SAVE_DIR=/mnt/nfs/Users/kazuki/checkpoints/llama/hf_checkpoints/Llama-2-70b-extended/iter_${FORMATTED_ITERATION}
 mkdir -p ${SAVE_DIR}
+
+# change latest_checkpointed_iteration.txt
+echo $ITERATION >/mnt/nfs/Users/kazuki/checkpoints/llama/megatron_checkpoints/Llama-2-70b-extended/tp${BASE_TENSOR_PARALLEL_SIZE}-pp${BASE_PIPELINE_PARALLEL_SIZE}/latest_checkpointed_iteration.txt
 
 python scripts/abci/megatron_to_hf/llama_checkpoint_conversion.py \
   --convert_checkpoint_from_megatron_to_transformers \
-  --load_path /mnt/nfs/Users/kazuki/checkpoints/llama/llama-megatron-convert-checkpoint-hf/Llama-2-70b/tp${BASE_TENSOR_PARALLEL_SIZE}-pp${BASE_PIPELINE_PARALLEL_SIZE} \
+  --load_path /mnt/nfs/Users/kazuki/checkpoints/llama/megatron_checkpoints/Llama-2-70b-extended/tp${BASE_TENSOR_PARALLEL_SIZE}-pp${BASE_PIPELINE_PARALLEL_SIZE} \
   --save_path $SAVE_DIR \
-  --target_params_dtype "fp16" \
+  --target_params_dtype "bf16" \
   --print-checkpoint-structure \
   --megatron-path /home/kazuki/llama/Megatron-LM
