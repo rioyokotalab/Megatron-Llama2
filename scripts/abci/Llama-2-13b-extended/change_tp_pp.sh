edf1217a8ba58c021ba4cb82ec944f2746569a00
kazuki <kaz.tokyo.tech20@gmail.com>
2023-11-11 16:25:38 +0900

    feat: 13b-base-extended-cc convert script

---
+++ scripts/abci/Llama-2-13b-extended/change_tp_pp.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#$ -l rt_AF=1
+#$ -l h_rt=5:00:00
+#$ -j y
+#$ -o outputs/change_tp_pp/
+#$ -cwd
+
+set -e
+
+# module load
+source /etc/profile.d/modules.sh
+module load cuda/11.8/11.8.0
+module load cudnn/8.9/8.9.2
+module load nccl/2.16/2.16.2-1
+module load hpcx/2.12
+
+# python virtualenv
+cd /bb/llm/gaf51275/llama/Megatron-LM
+source .env/bin/activate
+
+# distributed settings
+TARGET_TENSOR_PARALLEL_SIZE=1   # fixed
+TARGET_PIPELINE_PARALLEL_SIZE=1 # fixed
+
+BASE_TENSOR_PARALLEL_SIZE=2  # Llama-2 13B extended
+BASE_PIPELINE_PARALLEL_SIZE=4  # Llama-2 13B extended
+
+# model config
+BASE_CHECKPOINT_DIR=/bb/llm/gaf51275/llama/from_megatron_hf_checkpoints/megatron_checkpoints/Llama2-13b-base-extended-cc/tp${BASE_TENSOR_PARALLEL_SIZE}-pp${BASE_PIPELINE_PARALLEL_SIZE}
+TARGET_CHECKPOINT_DIR=/bb/llm/gaf51275/llama/from_megatron_hf_checkpoints/megatron_checkpoints/Llama2-13b-base-extended-cc/tp${TARGET_TENSOR_PARALLEL_SIZE}-pp${TARGET_PIPELINE_PARALLEL_SIZE}
+
+mkdir -p ${TARGET_CHECKPOINT_DIR}
+
+# tokenizer config
+TOKENIZER_MODEL=/bb/llm/gaf51275/jalm/jalm-tokenizer-private/tokenizer/jalm_llama_okazaki_lab_cc_nfkc_16k_aligned_8/merged_tokenizer_sp/jalm_llama.model
+
+# change latest_checkpointed_iteration.txt
+ITERATION=2000
+echo $ITERATION >${BASE_CHECKPOINT_DIR}/latest_checkpointed_iteration.txt
+
+# convert
+python tools/checkpoint/util.py \
+  --model-type GPT \
+  --loader megatron \
+  --saver megatron \
+  --megatron-path /bb/llm/gaf51275/llama/Megatron-LM \
+  --target-tensor-parallel-size ${TARGET_TENSOR_PARALLEL_SIZE} \
+  --target-pipeline-parallel-size ${TARGET_PIPELINE_PARALLEL_SIZE} \
+  --load-dir ${BASE_CHECKPOINT_DIR} \
+  --save-dir ${TARGET_CHECKPOINT_DIR}
