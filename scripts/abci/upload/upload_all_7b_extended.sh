86dc45998799ac6246bcc8aab4c3bd451927b9cb
kazuki <kaz.tokyo.tech20@gmail.com>
2023-11-16 18:49:23 +0900

    feat: 7b extended model

---
+++ scripts/abci/upload/upload_all_7b_extended.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+start=5000
+end=15000
+increment=5000
+
+tokenizer_path=/bb/llm/gaf51275/jalm/jalm-tokenizer-private/tokenizer/jalm_llama_okazaki_lab_cc_nfkc_16k_aligned_8/merged_tokenizer_sp/jalm_llama.model
+
+upload_base_dir=/bb/llm/gaf51275/llama/from_megatron_hf_checkpoints/hf_checkpoints/Llama2-7b-base-extended/okazaki_lab_cc
+
+for ((i = start; i <= end; i += increment)); do
+  upload_dir=$upload_base_dir/iter_$(printf "%07d" $i)
+
+  python scripts/abci/upload/upload.py \
+    --ckpt-path $upload_dir \
+    --repo-name tokyotech-llm/Llama2-7b-base-extended-megatron-iter$(printf "%07d" $i)
+done
