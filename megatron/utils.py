d368a0134270cfb18a80782f722f45acb9d129cd
kazuki <kaz.tokyo.tech20@gmail.com>
2023-12-13 15:50:40 +0000

    fix: tflops calculation

--- megatron/utils.py
+++ megatron/utils.py
@@ -263,14 +263,22 @@ def throughput_calculator(args, iteration_time, total_iterations):
     checkpoint_activations_factor = 3
     if hasattr(args, 'checkpoint_activations') and args.checkpoint_activations:
         checkpoint_activations_factor = 4
-    if hasattr(args, 'recompute_granularity') and (args.recompute_granularity == 'selective' or args.recompute_granularity == 'full'):
+    if hasattr(args, 'recompute_granularity') and (args.recompute_granularity == 'full'):
         checkpoint_activations_factor = 4
+    if hasattr(args, 'recompute_granularity') and (args.recompute_granularity == 'selective'):
+        checkpoint_activations_factor = 3.5
 
     seq_len: int = args.seq_length
     if hasattr(args, 'actual_seq_length'):
         seq_len: int = args.actual_seq_length
 
-    flops_per_iteration: float = ((8 + 4 * (intermediate_size / hidden_size)) * checkpoint_activations_factor * batch_size * seq_len * num_layers * (hidden_size**2)) * (1. + (seq_len / (6. * hidden_size)) + (vocab_size / (16. * num_layers * hidden_size)))
+    activation_function_factor: int = 4  # GELU
+    if args.swiglu:
+        activation_function_factor = 4 + 2  # SWiGLU (upscaling + down scaling)
+
+    flops_per_iteration: float = (
+        (8 + activation_function_factor * (intermediate_size / hidden_size)) * checkpoint_activations_factor * batch_size * seq_len * num_layers * (hidden_size**2)
+    ) * (1. + (seq_len / (6. * hidden_size)) + (vocab_size / (16. * num_layers * hidden_size)))
     tflops: float = flops_per_iteration / (elapsed_time_per_iter * args.world_size * (10**12))
 
     return samples_per_second, tflops, samples_per_model, model_replica_count
